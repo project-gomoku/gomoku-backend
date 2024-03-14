052fdf897298da7cc7235703fb8a3071f8a0253e
kimbeomjoon99 <tnldls99@naver.com>
2024-03-14 16:48:27 +0900

    Implement: implement common enumeration

--- src/main/java/com/gomoku/common/enumeration/Status.java
+++ src/main/java/com/gomoku/common/enumeration/Status.java
@@ -1,4 +1,22 @@
 package com.gomoku.common.enumeration;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
 public enum Status {
+    STARTED(1),
+    BLACK_TURN_IN_PROGRESS(2),
+    WHITE_TURN_IN_PROGRESS(3),
+    BLACK_WIN_NORMAL(4),
+    WHITE_WIN_NORMAL(5),
+    BLACK_WIN_TIME(6),
+    WHITE_WIN_TIME(7),
+    BLACK_WIN_RESIGN(8),
+    WHITE_WIN_RESIGN(9),
+    INTERRUPTED(0);
+
+    private final int status;
+
 }
--- src/main/java/com/gomoku/common/enumeration/UserType.java
+++ src/main/java/com/gomoku/common/enumeration/UserType.java
@@ -1,4 +1,13 @@
 package com.gomoku.common.enumeration;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
 public enum UserType {
+    GUEST(0),
+    NORMAL(1);
+    
+    private final int type;
 }
