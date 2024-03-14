57f8233ac9c4a83008081476cb71ba3c9eaff986
kimbeomjoon99 <tnldls99@naver.com>
2024-03-14 15:41:30 +0900

    Add: add entry point of application

---
+++ src/main/java/com/gomoku/GomokuApplication.java
@@ -0,0 +1,15 @@
+package com.gomoku;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@SpringBootApplication
+@EnableJpaAuditing
+public class GomokuApplication {
+
+    public static void main(String[] args){
+        SpringApplication.run(GomokuApplication.class);
+    }
+
+}
