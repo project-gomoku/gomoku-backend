f0c9db84ea7c1c36e7a00e50a27cceea3e8aad09
kimbeomjoon99 <tnldls99@naver.com>
2024-03-14 16:48:47 +0900

    Implement: implement common entities

--- src/main/java/com/gomoku/common/entity/User.java
+++ src/main/java/com/gomoku/common/entity/User.java
@@ -1,4 +1,30 @@
 package com.gomoku.common.entity;
 
+import com.gomoku.common.enumeration.UserType;
+import jakarta.persistence.*;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@Entity
 public class User extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(length = 10, nullable = false)
+    private String gameId;
+
+    @Column(nullable = false)
+    private int userType;
+
+    @Builder
+    public User(String gameId, UserType userType){
+        this.gameId = gameId;
+        this.userType = userType.getType();
+    }
+
 }
